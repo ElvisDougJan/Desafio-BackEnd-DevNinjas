e6452d4031236d5645c090eb93320d6406d59efb
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-06-08 20:39:33 -0300

    Trocando os nomes das rotas para ingles

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -3,15 +3,15 @@ const autenticacaoController = new AutenticacaoController()
 const { verificaToken } = require('./../utils/verificaToken')
 
 module.exports = app => {
-  app.post('/usuario', (req, res) => {
+  app.post('/user', (req, res) => {
     autenticacaoController.cadastraUsuarioLogin(req, res)
   })
 
-  app.get('/usuario', (req, res) => {
+  app.get('/user', (req, res) => {
     autenticacaoController.consultaUsuarios(req, res)
   })
 
-  app.get('/usuario/:id', verificaToken, (req, res) => {
+  app.get('/user/:id', verificaToken, (req, res) => {
     autenticacaoController.consultaUsuarioPorId(req, res)
   })
 } 
