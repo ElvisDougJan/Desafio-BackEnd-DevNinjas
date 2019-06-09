ab2e5620efe4a89c066c2773d033bc52aead2a99
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-06-09 17:21:48 -0300

    Adicionando validação para valores menores que 0

--- src/controllers/OrdersController.js
+++ src/controllers/OrdersController.js
@@ -5,6 +5,13 @@ class OrdersController {
   async createNewOrder(req, res) {
     const { id, created_at, status, total, buyer } = req.body
 
+    if (total < 0) {
+      res.status(400).json({
+        success: false,
+        message: 'Total value must be greater than 0.'
+      })
+    }
+
     const newOrder = {
       id,
       created_at: moment(created_at).format('YYYY-MM-DD HH:mm:ss'),
@@ -13,26 +20,36 @@ class OrdersController {
       customer_id: buyer.id
     }
 
-    const listItems = req.body.items.map(item => ({
-      amount: item.amount,
-      price_unit: item.price_unit,
-      total: item.total,
-      product_id: item.product.id,
-      order_id: req.body.id
-    }))
+    const everyValuesOK = req.body.items.every(item =>
+      item.price_unit < 0 || item.total < 0 || item.amount < 0 ? false : true)
 
-    return table('orders')
-      .insert(newOrder)
-      .then(async () => {
-        return table('items')
-          .insert(listItems)
-          .then(() => res.status(200).json({
-            success: true,
-            message: 'Order created successfully!'
-          }))
-          .catch(err => console.log(err))
+    if (!everyValuesOK) {
+      res.json({
+        success: false,
+        message: 'Every values must be greater than 0.'
       })
-      .catch(err => res.status(400).json(err))
+    } else {
+      const listItems = req.body.items.map(item => ({
+        amount: item.amount,
+        price_unit: item.price_unit,
+        total: item.total,
+        product_id: item.product.id,
+        order_id: req.body.id
+      }))
+
+      return table('orders')
+        .insert(newOrder)
+        .then(async () => {
+          return table('items')
+            .insert(listItems)
+            .then(() => res.status(200).json({
+              success: true,
+              message: 'Order created successfully!'
+            }))
+            .catch(err => new Error(err))
+        })
+        .catch(err => res.status(400).json(err))
+    }
   }
 }
 
